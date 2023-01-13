3bc3adfe334222b6477c6eaed84bb46d712399b4
Mahmodul Hasan <nicesanto92@gmail.com>
2023-01-13 14:52:30 +0600

    initializing setup

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { Typography } from "@mui/material";
+const App = () => {
+  return (
+    <div>
+      <Typography variant="h3">
+        Hello Universe.Thank You for everything you have done still now
+      </Typography>
+    </div>
+  );
+};
+
+export default App;
